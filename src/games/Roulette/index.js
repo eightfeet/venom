529133b557012fcc80e81042e760c01563ec660d
eightfeet <bajiao000000@163.com>
2019-07-19 17:03:31 +0800

    update

--- src/games/Roulette/index.js
+++ src/games/Roulette/index.js
@@ -6,21 +6,22 @@ import Core from '../Core';
 import { Loading, AddressModal, NoticeModal, validate, Message, Modal, htmlFactory } from '@byhealth/walle';
 const { inlineStyle } = htmlFactory;
 
-// import { renderGame } from './template';
+import { renderGame } from './template';
 import s from './game.scss';
 
 
 class Game {
 	constructor(config){
+		const { style, prizes } = config;
 		this.game = new Core({...config, lotteryForms: {
-			template: this.template(),
+			template: this.template(style.GameTheme, prizes),
 			launcher: `.${s.lotterybutton}`,
 			action: this.lottery
 		}});
 		this.Loading = this.game.Loading;
 	}
 
-	template = () => (`<div class="${s.lotterybutton}">测试</div>`)
+	template = renderGame
 
 	lottery = () => new Promise((resolve) => {
 		resolve();
