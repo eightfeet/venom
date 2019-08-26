c355523c60e91a00f9a420e74ee7e8d17379df28
eightfeet <bajiao000000@163.com>
2019-08-26 19:35:23 +0800

    update

--- src/games/Dice/creatfontface.js
+++ src/games/Dice/creatfontface.js
@@ -3,16 +3,7 @@ import svg from './fonts/dice.svg';
 import ttf from './fonts/dice.ttf';
 import woff from './fonts/dice.woff';
 
-const fontface = `@font-face {
-    font-family: 'dice';
-    src:  url('${eot}?7oj4ak');
-    src:  url('${eot}?7oj4ak#iefix') format('embedded-opentype'),
-      url('${ttf}?7oj4ak') format('truetype'),
-      url('${woff}?7oj4ak') format('woff'),
-      url('${svg}?7oj4ak#dice') format('svg');
-    font-weight: normal;
-    font-style: normal;
-}`;
+const fontface = ``;
 
 const styleDom = document.createElement('style');
 styleDom.setAttribute('type', 'text/css');
